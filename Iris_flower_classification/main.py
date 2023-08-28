1c5a7a4b8f01573d5691475d38f6c394966a417e
Gargi Chakraborty <110287003+GargiChakraborty105@users.noreply.github.com>
2023-08-28 17:35:37 +0530

    Update main.py

--- Iris_flower_classification/main.py
+++ Iris_flower_classification/main.py
@@ -13,8 +13,6 @@ data = pd.read_csv('IRIS.csv')
 X = data.drop('species', axis=1)
 y = data['species']
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Create and train the SVM model
 model = SVC(kernel='linear', C=1.0)
