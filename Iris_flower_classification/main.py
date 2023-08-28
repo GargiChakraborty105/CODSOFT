7a9e29e4c1d25d902a391616c6a3ad68b213f9a8
Gargi Chakraborty <110287003+GargiChakraborty105@users.noreply.github.com>
2023-08-28 17:34:44 +0530

    Update main.py

--- Iris_flower_classification/main.py
+++ Iris_flower_classification/main.py
@@ -1,16 +1,13 @@
 import pandas as pd
-import tkinter as tk
-from tkinter import ttk
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score, classification_report
+import tkinter as tk
+from tkinter import ttk
 from sklearn.datasets import load_iris
+
 # Load the dataset
 data = pd.read_csv('IRIS.csv')
-# Load the Iris dataset
-iris = load_iris()
-X = iris.data
-y = iris.target
 
 # Split into features (X) and labels (y)
 X = data.drop('species', axis=1)
@@ -34,6 +31,11 @@ target_names = data['species'].unique()
 print(classification_report(y_test, y_pred, target_names=target_names))
 
 
+
+# Load the Iris dataset
+iris = load_iris()
+X = iris.data
+y = iris.target
 
 # Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
